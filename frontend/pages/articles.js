89d06a066a8b4f3746c52f145f4ecb169e422554
random-long-int <pierrelouislet@gmail.com>
2022-10-19 21:17:55 +0200

    feat: adding nav to articles and contact pages

--- frontend/pages/articles.js
+++ frontend/pages/articles.js
@@ -1,3 +1,15 @@
-export default function Articles() {
-  return <h1>Articles</h1>;
+import Navbar from "../components/Navbar";
+
+export default function Contact() {
+  return (
+    <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
+      <section className="min-h-screen">
+        <Navbar />
+
+        <h2 className="text-2xl font-bold text-teal-800">
+          ARTICLES PAGE
+        </h2>
+      </section>
+    </main>
+  );
 }
--- frontend/pages/contact.js
+++ frontend/pages/contact.js
@@ -1,3 +1,16 @@
+// https://www.youtube.com/watch?v=e595v_M60W4&list=PLYmDjL9mFsW8uKDn4M6lEpT99XA_-nXrR&index=1
+import Navbar from "../components/Navbar";
+
 export default function Contact() {
-  return <h1>Contact</h1>;
+  return (
+    <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
+      <section className="min-h-screen">
+        <Navbar />
+
+        <h2 className="text-2xl font-bold text-teal-800">
+          CONTACT PAGE
+        </h2>
+      </section>
+    </main>
+  );
 }

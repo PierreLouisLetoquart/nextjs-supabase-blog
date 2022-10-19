ae2da9d85198341a25e952275f14e9beb434ffa4
random-long-int <pierrelouislet@gmail.com>
2022-10-19 21:27:00 +0200

    fix: home page title

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
           <Navbar/>
           <div className="text-center p-10 py-10">
             <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
-              ECE Webapp
+              Webapp
             </h2>
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
               Being smart and efficient

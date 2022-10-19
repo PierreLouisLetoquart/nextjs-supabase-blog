40670109ad076d48a81adefc2b143da9b79d44a8
random-long-int <pierrelouislet@gmail.com>
2022-10-19 21:17:04 +0200

    feat: adding about page content

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -1,3 +1,71 @@
+/* eslint-disable react/no-unescaped-entities */
+import { BoltIcon, ChatBubbleBottomCenterTextIcon, GlobeAltIcon, ScaleIcon } from '@heroicons/react/24/outline'
+import Navbar from '../components/Navbar';
+
+const features = [
+  {
+    name: 'Reconnu dans la commu',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: GlobeAltIcon,
+  },
+  {
+    name: 'Aimé de robotxt',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: ScaleIcon,
+  },
+  {
+    name: 'Très rapide et robuste',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: BoltIcon,
+  },
+  {
+    name: 'Mobile et tablette friendly',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: ChatBubbleBottomCenterTextIcon,
+  },
+]
+
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
+      <section className="min-h-screen">
+        <Navbar />
+        <div className="bg-white py-5">
+          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div className="lg:text-center">
+              <h2 className="text-lg font-semibold text-teal-600">About</h2>
+              <p className="mt-2 text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">
+                Pourquoi ce site ?
+              </p>
+              <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+                le but de ce site est d'apprendre a utiliser le Framework Nextjs de la librairie Reactjs. Nous utiliserons
+                aussi Tailwindcss.
+              </p>
+            </div>
+
+            <div className="mt-10">
+              <dl className="space-y-10 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10 md:space-y-0">
+                {features.map((feature) => (
+                  <div key={feature.name} className="relative">
+                    <dt>
+                      <div className="absolute flex h-12 w-12 items-center justify-center rounded-md bg-teal-500 text-white">
+                        <feature.icon className="h-6 w-6" aria-hidden="true" />
+                      </div>
+                      <p className="ml-16 text-lg font-medium leading-6 text-gray-900">{feature.name}</p>
+                    </dt>
+                    <dd className="mt-2 ml-16 text-base text-gray-500">{feature.description}</dd>
+                  </div>
+                ))}
+              </dl>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+
+  )
 }

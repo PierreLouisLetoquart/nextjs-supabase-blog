aba1791afbaa0ddcf4dff9d47edef370f404a592
random-long-int <pierrelouislet@gmail.com>
2022-10-24 18:34:47 +0200

    fix: update layout paddings for better display

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 
 export default function Layout({children}) {
     return (
-        <div>
+        <div className="px-8 md:px-18 lg:px-40">
             <Navbar />
             <main>{children}</main>
         </div>

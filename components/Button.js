e63aca6af9ddc88753998487632a216494101155
random-long-int <pierrelouislet@gmail.com>
2022-12-17 20:53:41 +0100

    clean up repo for project

--- components/Button.js
+++
@@ -1,7 +0,0 @@
-export default function Button({ children }) {
-    return (
-        <button className='bg-greenCode px-3 py-1 rounded-md'>
-            <span className='text-white text-base md:text-lg font-semibold'>{children}</span>
-        </button>
-    )
-}
--- components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <div className="w-full h-28 flex justify-center items-center text-white text-xs md:text-lg">
-            <h4>&#169;Geek<span className="text-[#008000]">it.</span> | <span className="text-cyan-800">bool</span> free_web3_community = <span className="text-yellow-500">true</span>;</h4>
-        </div>
-    );
-}

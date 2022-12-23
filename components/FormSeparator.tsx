920648c21a21e0a922cbd69602fb56f590847d08
random-long-int <pierrelouislet@gmail.com>
2022-12-24 00:35:11 +0100

    Clean repo

--- components/FormSeparator.tsx
+++
@@ -1,9 +0,0 @@
-export default function Separator() {
-    return (
-        <div className='w-full flex items-center justify-center'>
-            <div className='w-1/5 h-0.5 bg-zinc-300'></div>
-            <p className='text-zinc-400 px-2 font-bold'>or</p>
-            <div className='w-1/5 h-0.5 bg-zinc-300'></div>
-        </div>
-    )
-}

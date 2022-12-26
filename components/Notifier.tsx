846070bc19bee7f6c092127d99e3606e2203c67b
Pierre-Louis Létoquart <pierre-louisletoquart@Macito.local>
2022-12-26 19:28:33 +0100

    feat: 💭 add notification componant 💭 - fix indentation

---
+++ components/Notifier.tsx
@@ -0,0 +1,29 @@
+import { ToastContainer, ToastOptions } from 'react-toastify';
+
+export const Notifier = () => {
+    return (
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
+    )
+}
+
+export const NotifierProps : ToastOptions = {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,15 +6,16 @@ import ThemeProvider from '../contexts/theme-provider';
 import { useAuth } from '../hooks/useAuth';
 import { Notifier } from '../components/Notifier';
 
-export default function App({ Component, pageProps }: AppProps) {  
+export default function App({ Component, pageProps }: AppProps) {
 
-  const { session } = useAuth();  
-  return (    
-    <SupabaseProvider session={session}>      
-    <SupabaseListener serverAccessToken={session?.access_token} />        
-    <ThemeProvider>        
-       <Component {...pageProps} />
-    </ThemeProvider>    
-    </SupabaseProvider>  
+  const { session } = useAuth();
+  return (
+    <SupabaseProvider session={session}>
+      <SupabaseListener serverAccessToken={session?.access_token} />
+      <ThemeProvider>
+        <Notifier />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SupabaseProvider>
   )
 }

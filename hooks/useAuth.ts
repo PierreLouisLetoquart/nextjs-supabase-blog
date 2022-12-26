b328a54b73cc95afe494718f9025dd755d215ebe
KilanJhin <kylan.kardous@gmail.com>
2022-12-26 19:19:45 +0100

    feat: add auth hook to use supabase function - add custom hook useAuth - add provider for supabase

---
+++ hooks/useAuth.ts
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react'
+import { createBrowserClient } from '../utils/supabase-browser'
+import type { Session } from '@supabase/supabase-js'
+
+export const useAuth = () => {    
+  const supabase = createBrowserClient()    
+  const [session, setSession] = useState<Session | null>(null)    
+  const [error, setError] = useState<any>(null)    
+
+  useEffect(() => {        
+    const fetchData = async () => {            
+       try {                
+          const { data : { session } } = await supabase.auth.getSession()  
+              setSession(session)            
+        } catch (error) {                
+          setError(error)            
+        }        
+      }        
+      fetchData()    }, [])    
+  return { session, error };
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import ThemeProvider from '../contexts/theme-provider'
+import SupabaseListener from '../contexts/supabase-listener'
+import SupabaseProvider from '../contexts/supabase-provider'
+import ThemeProvider from '../contexts/theme-provider';
+import { useAuth } from '../hooks/useAuth';
+import { Notifier } from '../components/Notifier';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
+export default function App({ Component, pageProps }: AppProps) {  
+
+  const { session } = useAuth();  
+  return (    
+    <SupabaseProvider session={session}>      
+    <SupabaseListener serverAccessToken={session?.access_token} />        
+    <ThemeProvider>        
+       <Component {...pageProps} />
+    </ThemeProvider>    
+    </SupabaseProvider>  
   )
 }

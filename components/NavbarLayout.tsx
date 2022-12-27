880c27a078209ab66db06956fcdcdedc073e520f
KilanJhin <kylan.kardous@gmail.com>
2022-12-27 18:57:44 +0100

    feat: 🔠 add navbar for entire app 🔠 - add layout for entire app - diffrent nav buttons depends on pathname - theme toggler

---
+++ components/NavbarLayout.tsx
@@ -0,0 +1,99 @@
+import Link from "next/link"
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useTheme } from "../contexts/theme-provider";
+import { useSupabase } from "../contexts/supabase-provider";
+import { motion as m } from 'framer-motion';
+
+export default function NavbarLayout({ children } : { children: React.ReactNode }) {
+    const { theme } = useTheme();
+
+    return (
+        <main className={`w-full min-h-screen ${theme === 'dark' ? 'bg-zinc-900 selection:text-zinc-900 selection:bg-white' : 'bg-zinc-50 selection:text-zinc-50 selection:bg-zinc-900'}`}>
+            <div className={`fixed top-0 left-0 z-10 w-full`}>
+                <Navbar />
+            </div>
+            {children}
+        </main>
+    )
+}
+
+function Navbar() {
+    const { theme } = useTheme();
+    const { session } = useSupabase();
+    
+    return (
+        <nav className={`w-full h-20 backdrop-blur-md px-4 md:px-6 lg:px-12 border-b-[1px] ${theme === 'dark' ? 'border-zinc-800' : 'border-zinc-200'}`}>
+            <div className="w-full max-w-6xl mx-auto h-full flex justify-between items-center">
+                <Link href={`/`}>
+                    <h1 className={`text-2xl font-extrabold tracking-wide ${theme === 'dark' ? 'text-zinc-50' : 'text-zinc-900'}`}>
+                        Geekit.
+                    </h1>
+                </Link>
+                <section className="flex items-center gap-6">
+                    <div className={`pr-6 py-2 border-r-[1px] ${theme === 'dark' ? 'border-zinc-800' : 'border-zinc-200'}`}>
+                        <Toggler />
+                    </div>
+                    <NavButton />
+                </section>
+            </div>
+        </nav>
+    )
+}
+
+export function NavButton() {
+    const { theme } = useTheme();
+    const router = useRouter();
+
+    const { session } = useSupabase();
+    const [path, setPath] = useState<string>('/');
+    const [text, setText] = useState<string>('');
+
+    useEffect(() => {
+        if(session && router.pathname === '/') {
+           setPath('/blog');
+           setText('Browse');
+        } else if(!session && router.pathname === '/') {
+            setPath('/login');
+            setText('Login');
+        } else if(router.pathname.startsWith('/blog')) {
+            setPath(`/profile/${session?.user.id}`);
+            setText('Profile');
+        } else if(router.pathname.startsWith('/profile')) {
+            setPath('/blog');
+            setText('Feed');
+        } else if(router.pathname.startsWith('/login')) {
+            setPath('/');
+            setText('Home');
+        } else {
+            setPath('/');
+            setText('Home');
+        }
+    }, [router.pathname])
+
+    return (
+        <Link href={path}>
+            <button className={`px-4 py-[6px] rounded-full tracking-wide flex justify-center items-center ${theme === 'dark' ? 'bg-zinc-50' : 'bg-zinc-900'}`}>
+                <span className={`text-xs md:text-sm font-semibold ${theme === 'dark' ? 'text-zinc-900' : 'text-zinc-50'}`}>
+                    {text}
+                </span>
+            </button>
+        </Link>
+    )
+}
+
+export function Toggler() {
+    const { theme, setTheme } = useTheme();
+
+    return (
+        <div 
+            className={`w-7 h-4 flex rounded-full p-[2px] cursor-pointer ${theme == "dark" ? 'justify-end bg-zinc-100' : 'justify-start bg-zinc-900'}`} 
+            onClick={() => setTheme(theme == "dark" ? "light" : "dark")}
+        >
+            <m.div 
+                className={`h-3 w-3 rounded-full ${theme == "dark" ? 'bg-zinc-900' : 'bg-zinc-100'}`} 
+                layout transition={{ type: "spring", stiffness: 700, damping: 30 }} 
+            />
+        </div>
+    )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,18 +4,19 @@ import SupabaseListener from '../contexts/supabase-listener'
 import SupabaseProvider from '../contexts/supabase-provider'
 import ThemeProvider from '../contexts/theme-provider';
 import { useAuth } from '../hooks/useAuth';
-import { Notifier } from '../components/Notifier';
+import NavbarLayout from '../components/NavbarLayout';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {  
 
-  const { session } = useAuth();
-  return (
-    <SupabaseProvider session={session}>
-      <SupabaseListener serverAccessToken={session?.access_token} />
-      <ThemeProvider>
-        <Notifier />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </SupabaseProvider>
+  const { session } = useAuth();  
+  return (    
+    <SupabaseProvider session={session}>      
+    <SupabaseListener serverAccessToken={session?.access_token} />        
+    <ThemeProvider>        
+       <NavbarLayout>
+       <Component {...pageProps} />
+       </NavbarLayout>
+    </ThemeProvider>    
+    </SupabaseProvider>  
   )
 }

ca7a03f025472e63d05aa2c51f7a3894a1bb437e
Pierre-Louis Létoquart <pierre-louisletoquart@macito.home>
2022-12-28 15:43:50 +0100

    feat: 📵 add message for profile without posts created or saved 📵

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import { PostModifier } from "../../components/PostModifier";
 import ProfileLayout from "../../components/ProfileLayout";
+import { useTheme } from "../../contexts/theme-provider";
 import supabase from "../../utils/supabase";
+import { IoDocumentTextOutline } from 'react-icons/io5';
+import { useRouter } from "next/navigation";
 
 export default function Profile({posts} : {posts: any}) {
     const [postsList, setPostsList] = useState(posts);
 
+    if(!postsList) return <ProfileLayout><NoPostBanner /></ProfileLayout>
     return (
         <>
             <ProfileLayout>
@@ -33,4 +37,26 @@ export async function getServerSideProps({ params } : any) {
             posts
         }
     }
+}
+
+function NoPostBanner() {
+    const { theme } = useTheme();
+    const router = useRouter();
+
+    return (
+        <section className="w-full pt-20">
+            <section className="w-full max-w-xl mx-auto flex flex-col items-center gap-4">
+                <div className="flex gap-2 items-center">
+                    <IoDocumentTextOutline className="text-2xl text-zinc-600" />
+                    <h1 className="text-2xl font-semibold text-zinc-600">No posts yet</h1>
+                </div>
+                <p className="text-zinc-600">When you post something, it will appear here.</p>
+                <button onClick={() => router.push('/blog/new')} className={`mt-3 px-4 py-[6px] rounded-full tracking-wide flex justify-center items-center ${theme === 'dark' ? 'bg-zinc-400' : 'bg-zinc-500'}`}>
+                    <span className={`text-xs md:text-sm font-semibold ${theme === 'dark' ? 'text-zinc-900' : 'text-zinc-50'}`}>
+                        Create a post
+                    </span>
+                </button>
+            </section>
+        </section>
+    )
 }
--- pages/profile/saved/[id].tsx
+++ pages/profile/saved/[id].tsx
@@ -1,11 +1,15 @@
+import { useRouter } from "next/router";
 import { useState } from "react";
+import { IoDocumentTextOutline } from "react-icons/io5";
 import { PostCard } from "../../../components/PostCard";
 import ProfileLayout from "../../../components/ProfileLayout";
+import { useTheme } from "../../../contexts/theme-provider";
 import supabase from "../../../utils/supabase";
 
 export default function Profile({bookmarks} : {bookmarks: any}) {
     const [bookmarksList, setBookmarksList] = useState(bookmarks);
 
+    if(!bookmarksList) return <ProfileLayout><NoPostBanner /></ProfileLayout>
     return (
         <>
             <ProfileLayout>
@@ -32,4 +36,26 @@ export async function getServerSideProps({ params } : any) {
             bookmarks
         }
     }
+}
+
+function NoPostBanner() {
+    const { theme } = useTheme();
+    const router = useRouter();
+
+    return (
+        <section className="w-full pt-20">
+            <section className="w-full max-w-xl mx-auto flex flex-col items-center gap-4">
+                <div className="flex gap-2 items-center">
+                    <IoDocumentTextOutline className="text-2xl text-zinc-600" />
+                    <h1 className="text-2xl font-semibold text-zinc-600">No posts</h1>
+                </div>
+                <p className="text-zinc-600">When you save something, it will appear here.</p>
+                <button onClick={() => router.push('/blog')} className={`mt-3 px-4 py-[6px] rounded-full tracking-wide flex justify-center items-center ${theme === 'dark' ? 'bg-zinc-400' : 'bg-zinc-500'}`}>
+                    <span className={`text-xs md:text-sm font-semibold ${theme === 'dark' ? 'text-zinc-900' : 'text-zinc-50'}`}>
+                        Explore
+                    </span>
+                </button>
+            </section>
+        </section>
+    )
 }
